4d92d6c02ee5b9144eacc3d58617ff73c11b6444
GhitaB <ghitabizau27@gmail.com>
2022-05-05 11:23:42 +0300

    WIP download translated file.

--- download_file.py
+++ download_file.py
@@ -1,13 +1,19 @@
-import datetime
+""" Download files from eTranslation
+"""
 import logging
-from config import cookies, headers, UPLOAD_URL, UPLOAD_FORM_DATA
+import requests
+from secret_config import COOKIES
+from config import HEADERS, TARGET_LANGUAGE
 
 def download_translated(filename):
+    """ Download a translated file from eTranslation
+    """
+    client_request_id = "111" # TODO extract it
     logger = logging.getLogger(__name__)
     url = 'https://webgate.ec.europa.eu/etranslation/download.html'
     form_data = {
         'clientRequestId': client_request_id,
-        'targetLanguage': 'RO', # TODO extract language
+        'targetLanguage': TARGET_LANGUAGE,
         'targetFileName': filename,
         'targetFormat': 'html',
         'deleteFile': 'false'
@@ -15,8 +21,8 @@ def download_translated(filename):
     response = requests.post(
         url,
         form_data,
-        headers=headers,
-        cookies=cookies,
+        headers=HEADERS,
+        cookies=COOKIES,
     )
 
     if response.status_code == 200:
@@ -24,10 +30,30 @@ def download_translated(filename):
             logger.warning("0 bytes response. Check clientRequestId.")
             return 'pending'
         response.raise_for_status() # ensure we notice bad responses
-        file = open('./archive/' + filename, "wb")
+        file = open('./translated_files/' + filename, "wb")
         file.write(response.content)
         file.close()
         import pdb; pdb.set_trace()
         logger.info("Downloading... " + filename)
         return 'downloaded'
     return 'pending'
+
+def main():
+    """ Main
+    """
+    # Prepare
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
+    logger = logging.getLogger(__name__)
+
+    logger.info("We will try to download the translated file.")
+
+    # The file
+    filename_translated = 'EN-RO-2022_05_05-10_19_36-80E596E28826107A_RO.html'
+    logger.info("Filename: %s", filename_translated)
+
+    # Download
+    resp = download_translated(filename_translated)
+    logger.info("Status: %s", resp)
+
+if __name__ == '__main__':
+    main()
