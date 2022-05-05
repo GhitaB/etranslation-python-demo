37475c38e426d75bf1576c3f0d48fc214c307e67
GhitaB <ghitabizau27@gmail.com>
2022-05-05 12:58:00 +0300

    WIP extract files data.

--- download_file.py
+++ download_file.py
@@ -1,9 +1,13 @@
 """ Download files from eTranslation
 """
 import logging
+import re
 import requests
+from bs4 import BeautifulSoup
 from secret_config import COOKIES
 from config import HEADERS, TARGET_LANGUAGE
+
+files_data = {}
 
 def download_translated(filename):
     """ Download a translated file from eTranslation
@@ -36,6 +40,23 @@ def download_translated(filename):
         return 'downloaded'
     return 'pending'
 
+def extract_files_data():
+    """ Get files data from eTranslation table in 'My translation requests'
+    """
+    url = 'https://webgate.ec.europa.eu/etranslation/translationRequestHistory.html'
+    response = requests.get(
+        url,
+        headers=HEADERS,
+        cookies=COOKIES,
+    )
+    js_script = BeautifulSoup(response.content, features="html.parser").find_all("script")[-1]
+    pattern = re.compile('var\s+list\s+=\s+(.*);')
+
+    if(pattern.match(str(js_script.string))):
+        import pdb; pdb.set_trace()
+
+    import pdb; pdb.set_trace()
+
 def main():
     """ Main
     """
@@ -43,15 +64,18 @@ def main():
     logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
-    logger.info("We will try to download the translated file.")
+    # Extract files data
+    extract_files_data()
+
+    # logger.info("We will try to download the translated file.")
 
     # The file
-    filename_translated = 'EN-RO-2022_05_05-10_47_17-93FC10021805926F_RO.html'
-    logger.info("Filename: %s", filename_translated)
+    # filename_translated = 'EN-RO-2022_05_05-10_47_17-93FC10021805926F_RO.html'
+    # logger.info("Filename: %s", filename_translated)
 
     # Download
-    resp = download_translated(filename_translated)
-    logger.info("Status: %s", resp)
+    # resp = download_translated(filename_translated)
+    # logger.info("Status: %s", resp)
 
 if __name__ == '__main__':
     main()
